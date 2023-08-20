6cc29c336dc2d22d5fa0cc867994a549efcdc446
Shantanu <shantanuwable2003@gmail.com>
2023-08-20 16:18:48 +0530

    Created Controllers

---
+++ controllers/user.ts
@@ -0,0 +1,6 @@
+import * as dotenv from "dotenv";
+dotenv.config(); // Load environment variables from .env file
+import { Request, Response } from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import queryDatabase from "../database/connection";
