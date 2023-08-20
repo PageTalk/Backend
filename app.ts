8659788d96856679680323ece1a9370d287068a7
Shantanu <shantanuwable2003@gmail.com>
2023-08-20 16:19:14 +0530

    Added dotenv to app.ts

--- app.ts
+++ app.ts
@@ -1,7 +1,9 @@
-const express = require("express");
+import * as dotenv from "dotenv";
+dotenv.config(); // Load environment variables from .env file
+import express from "express";
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
