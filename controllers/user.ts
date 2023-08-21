5341818712b02a91036e4c9c2bb6b3f060ed655e
Shantanu Wable <shantanuwable2003@gmail.com>
2023-08-21 23:05:10 +0530

    Added testing queries

--- controllers/user.ts
+++ controllers/user.ts
@@ -4,3 +4,36 @@ import { Request, Response } from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import queryDatabase from "../database/connection";
+
+export const createUser = async (req: Request, res: Response) => {
+  const { username, first_name, last_name, email, password, role, phone } =
+    req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const getUserQuery = `
+  SELECT *
+  FROM users
+  WHERE email = $1`;
+
+  let retrieveUser = queryDatabase(getUserQuery, [email]);
+
+  console.log(retrieveUser);
+
+  // //Hashing the password using bcrypt
+  // const salt = await bcrypt.genSalt(10);
+  // const securedPassword = await bcrypt.hash(password, salt);
+
+  // const createUserQuery = `
+  // INSERT INTO
+  // `
+};
+
+// sample function to get all users
+export const getAllUsers = async (req: Request, res: Response) => {
+  const getAllUsersQuery = `
+  SELECT *
+  FROM users`;
+
+  const allUsers = await queryDatabase(getAllUsersQuery);
+
+  res.status(200).send(allUsers);
+};
