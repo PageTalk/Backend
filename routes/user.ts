59a66d78f507bb3bc4b58646bb35aa9b7c355cb2
Shantanu Wable <shantanuwable2003@gmail.com>
2023-08-21 23:05:26 +0530

    Added a get and post req

--- routes/user.ts
+++ routes/user.ts
@@ -1,6 +1,11 @@
 import express from "express";
 const router = express.Router();
 
+import { createUser, getAllUsers } from "../controllers/user";
+
+router.route("/create").post(createUser);
+router.route("/get").get(getAllUsers);
+
 export default router;
 
 // TODO: Add User routes
