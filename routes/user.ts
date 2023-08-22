0f1073ca3c8936e165bd91cbce79f0cd5db8236a
Shantanu <shantanuwable2003@gmail.com>
2023-08-22 23:17:41 +0530

    Added some routes

--- routes/user.ts
+++ routes/user.ts
@@ -1,10 +1,12 @@
 import express from "express";
 const router = express.Router();
 
-// import { createUser, getAllUsers } from "../controllers/user";
+import { createUser, getAllUsers, getNamedUsers } from "../controllers/user";
 
-// router.route("/create").post(createUser);
-// router.route("/get").get(getAllUsers);
+router.route("/create").post(createUser);
+router.route("/get").get(getAllUsers);
+router.route("/:name").get(getNamedUsers);
+router.route("/create").post(createUser);
 
 export default router;
 
