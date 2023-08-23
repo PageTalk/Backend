d66f89f8bf0fefdc3e762931ec33c6c6ede02dbf
Shantanu <shantanuwable2003@gmail.com>
2023-08-23 12:31:16 +0530

    Added a login query

--- controllers/user.ts
+++ controllers/user.ts
@@ -18,7 +18,7 @@ export const createUser = async (req: Request, res: Response) => {
 
     if (retrieveUser.length !== 0) {
       return res.status(400).json({
-        success: false,
+        status: false,
         message: "Sorry the user with the given email already exists",
       });
     }
@@ -48,28 +48,76 @@ export const createUser = async (req: Request, res: Response) => {
     // Creating a JWT token
     const authToken = jwt.sign(payloadData, process.env.JWT_SECRET!);
 
-    res.status(200).json({
-      success: true,
+    return res.status(200).json({
+      status: true,
       message: "User created successfully",
       authToken: authToken,
     });
+
   } catch (error) {
-    res.status(500).json({
+    return res.status(500).json({
+      status: false,
       message: "Some error occured while creating a user",
-      success: "false",
       error: error,
     });
   }
 };
 
 export const loginUser = async (req: Request, res: Response) => {
-  
-}
+  try {
+    const { username, password } = req.body;
+    const getUserQuery = `SELECT *
+		FROM users 
+		WHERE username = ?`;
 
+    let retrievedUserArray = await queryDatabase(getUserQuery, [username]);
 
+    if (!retrievedUserArray || retrievedUserArray.length === 0) {
+      return res.status(400).json({
+        status: false,
+        message: "No such user found",
+      });
+    }
 
+    const retrievedUser = retrievedUserArray[0];
 
+    const passwordCompare = await bcrypt.compare(
+      password,
+      retrievedUser.password
+    );
 
+    if (!passwordCompare) {
+      return res.status(401).json({
+        status: false,
+        message: "Login with correct password",
+      });
+    }
+
+    const payloadData = {
+      id: retrievedUser.id,
+      username: retrievedUser.username,
+      email: retrievedUser.email,
+    };
+
+    // Creating a JWT token
+    const authToken = jwt.sign(payloadData, process.env.JWT_SECRET!);
+
+    return res.status(201).json({
+      status: true,
+      message: "User Login successful",
+      authToken: authToken,
+    });
+
+  } catch (error) {
+    return res.status(500).json({
+      status: false,
+      message: "Some error occured",
+      error: error,
+    });
+  }
+};
+
+// =================== SAMPLE QUERIES ===================
 
 // sample function to get all users
 export const getAllUsers = async (req: Request, res: Response) => {
@@ -78,17 +126,17 @@ export const getAllUsers = async (req: Request, res: Response) => {
     const query = "SELECT * FROM users";
 
     const results = await queryDatabase(query);
-    res.status(200).json({
-      status: "success",
+    return res.status(200).json({
+      status: true,
       results: results.length,
       data: {
         users: results,
       },
     });
   } catch (error) {
-    res.status(500).json({
+    return res.status(500).json({
+      status: false,
       message: "Some error occured",
-      success: "false",
       error: error,
     });
   }
@@ -99,17 +147,17 @@ export const getNamedUsers = async (req: Request, res: Response) => {
   try {
     const query = "SELECT * FROM users WHERE first_name = ?";
     const results = await queryDatabase(query, [req.params.name]);
-    res.status(200).json({
-      status: "success",
+    return res.status(200).json({
+      status: true,
       results: results.length,
       data: {
         users: results,
       },
     });
   } catch (error) {
-    res.status(500).json({
+    return res.status(500).json({
+      status: false,
       message: "Some error occured",
-      success: "false",
       error: error,
     });
   }
