321d5caace44439d941841820bd4972fa1ce6394
Shantanu <shantanuwable2003@gmail.com>
2023-09-10 12:50:07 +0530

    Migrated from mysql to mysql2 Package

--- app.ts
+++ app.ts
@@ -1,8 +1,7 @@
 import * as dotenv from "dotenv";
 dotenv.config(); // Load environment variables from .env file
-import mysql from "mysql";
 import express from "express";
-import { initializeApp } from "firebase/app";
+import mysql, { ConnectionOptions } from "mysql2";
 
 import UserRouter from "./routes/user";
 import AdminRouter from "./routes/admin";
@@ -14,7 +13,7 @@ import CollectionRouter from "./routes/collection";
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json()); // Middleware
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 
 // Routes
 
@@ -28,26 +27,27 @@ app.use("/api/v1/collection", CollectionRouter);
 
 // Starting Server
 const start = async () => {
-  try {
-    const connection = mysql.createConnection({
-      host: process.env.DATABASE_HOST,
-      user: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_DATABASE,
-    });
-    await connection.connect((err: Error) => {
-      if (err) {
-        console.error("Error connecting to MySQL:", err);
-        return;
-      }
-      console.log("Connected to MySQL!");
-    });
-    app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
-    });
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        const connection = mysql.createConnection({
+            host: process.env.DATABASE_HOST,
+            port: 3306,
+            user: process.env.DATABASE_USER,
+            password: process.env.DATABASE_PASSWORD,
+            database: process.env.DATABASE_DATABASE,
+        });
+        await connection.connect((err) => {
+            if (err) {
+                console.error("Error connecting to MySQL:", err);
+                return;
+            }
+            console.log("Connected to MySQL!");
+        });
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 start();
