7e8c9c64ac9075950349f0107e5a6e8eb26938d6
Shantanu <shantanuwable2003@gmail.com>
2023-09-15 10:44:29 +0530

    AAdded comments, changed function name

--- routes/user.ts
+++ routes/user.ts
@@ -2,21 +2,21 @@ import express from "express";
 const router = express.Router();
 
 import {
-  createUser,
-  loginUser,
-  getAllUsers,
-  getNamedUsers,
-  updateUser,
+    createUser,
+    loginUser,
+    getAllUsers,
+    getUserByUsername,
+    updateUser,
 } from "../controllers/user";
 
-router.route("/get").get(getAllUsers);
-router.route("/login").post(loginUser);
-router
-  .route("/:username")
-  .get(getNamedUsers)
-  .post(createUser)
-  .patch(updateUser);
-  
+router.route("/get").get(getAllUsers);  // Get all users in the db
+router.route("/login").post(loginUser); //  login
+router  
+    .route("/:username")
+    .get(getUserByUsername) // Get user with that username
+    .post(createUser)       // Create user
+    .patch(updateUser);     // update user
+
 export default router;
 
 // TODO: Add User routes
