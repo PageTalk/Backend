474b721c4761c261feae39719d3b652d6238983b
Shantanu <shantanuwable2003@gmail.com>
2023-09-16 09:26:57 +0530

    Added routes to query

--- routes/query.ts
+++ routes/query.ts
@@ -1,6 +1,19 @@
 import express from "express";
 const router = express.Router();
 
+import {
+    sendQuery,
+    getAllQueriesbyUsername,
+    getQuerybyID,
+    updateQuery,
+    deleteQuery,
+    getAllQueries
+} from "../controllers/query";
+
+router.route("/:username").get(getAllQueriesbyUsername).patch(updateQuery).post(sendQuery).delete(deleteQuery);
+router.route("/:username/:id").get(getQuerybyID);
+router.route("/").get(getAllQueries);
+
 export default router;
 
 // TODO: Add Query routes
