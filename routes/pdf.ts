b9ca2e4550fdde65a8544192a9d5d5963e6b1e94
Shantanu Wable <shantanuwable2003@gmail.com>
2023-09-16 19:20:15 +0530

    Removed username from Route

--- routes/pdf.ts
+++ routes/pdf.ts
@@ -3,7 +3,7 @@ const router = express.Router();
 
 import { uploadPDF, uploadForm, retrievePDF } from "../controllers/pdf";
 
-router.route("/:username").get(uploadForm).post(uploadPDF);
+router.route("/").get(uploadForm).post(uploadPDF);
 router.route('/:username/all').get(retrievePDF);
 
 export default router;
