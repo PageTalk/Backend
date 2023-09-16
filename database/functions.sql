12f5db5a5e5f8097b17ca34448d8f5a35c8c8efe
Shantanu Wable <shantanuwable2003@gmail.com>
2023-09-16 10:44:07 +0530

    Added a function to insert PDF and return pdf_id

--- database/functions.sql
+++ database/functions.sql
@@ -18,3 +18,23 @@ BEGIN
 END //
 
 DELIMITER ;
+
+DELIMITER //
+
+CREATE FUNCTION InsertPDFAndGetID(
+    p_fk_user_id INT,
+    p_pdf_file VARCHAR(255)
+)
+RETURNS INT
+BEGIN
+    DECLARE new_pdf_id INT;
+    
+    INSERT INTO `pdf` (fk_user_id, pdf_file)
+    VALUES (p_fk_user_id, p_pdf_file);
+    
+    SET new_pdf_id = LAST_INSERT_ID();
+    
+    RETURN new_pdf_id;
+END //
+
+DELIMITER ;
